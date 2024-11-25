bb7ae7cbe0a3244881aaf5621224e3a86d41f3bc
Jérôme <jfreyheit@gmail.com>
2024-11-25 23:04:19 +0700

    Remove rate limiting

--- functions/invite/handleRequest.ts
+++ functions/invite/handleRequest.ts
@@ -5,19 +5,12 @@ import { getCorsHeaders } from "./corsHeaders"
 
 type HandleRequestProps<TCallbackOutput, TInput, TOutput, TIssue extends BaseIssue<unknown>, TSchema extends BaseSchema<TInput, TOutput, TIssue>> = {
   env: Env
-  rateLimiterToken?: string
   request: Request
   schema: TSchema
   callback: (params: InferOutput<TSchema>, env: Env) => Promise<TCallbackOutput>
 }
 
-export async function handleRequest<TCallbackOutput, TInput, TOutput, TIssue extends BaseIssue<unknown>, TSchema extends BaseSchema<TInput, TOutput, TIssue>>({ callback, env, rateLimiterToken, request, schema }: HandleRequestProps<TCallbackOutput, TInput, TOutput, TIssue, TSchema>): Promise<Response> {
-  const { pathname } = new URL(request.url)
-  const { success } = await env.MY_RATE_LIMITER.limit({ key: rateLimiterToken ?? pathname })
-  if (!success) {
-    return new Response("Rate Limit Exceeded", { status: 429 })
-  }
-
+export async function handleRequest<TCallbackOutput, TInput, TOutput, TIssue extends BaseIssue<unknown>, TSchema extends BaseSchema<TInput, TOutput, TIssue>>({ callback, env, request, schema }: HandleRequestProps<TCallbackOutput, TInput, TOutput, TIssue, TSchema>): Promise<Response> {
   const bodyContent = await request.json()
   const parseOutput = safeParse(schema, bodyContent)
 
