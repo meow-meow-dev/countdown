11dfc121ef5f5ee83f3fde420446cbea85d63484
Jérôme <jfreyheit@gmail.com>
2025-07-25 21:57:38 +0700

    fix: fix confettis triggering too early

--- src/client/Countdown.tsx
+++ src/client/Countdown.tsx
@@ -40,7 +40,6 @@ export function Countdown({
   const minutes = Math.floor(delta.as("minutes")) % 60;
   const seconds = totalSeconds % 60;
 
-  console.log({ seconds });
   return (
     <div
       className={twMerge(
@@ -64,7 +63,7 @@ export function Countdown({
         </div>
       </div>
 
-      {totalSeconds <= 60 * 185 ? (
+      {totalSeconds <= 0 ? (
         <Confetti />
       ) : (
         <Flex
